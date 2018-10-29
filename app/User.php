d02553f4935aa4ca7bb64b9b8a1032a347fb85b8
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2018-10-29 13:05:12 +0700

    Fix bug user pivot table

--- app/User.php
+++ app/User.php
@@ -33,7 +33,7 @@ class User extends Authenticatable
 
     public function roles()
     {
-        return $this->belongsToMany(Role::class);
+        return $this->belongsToMany(Role::class)->withTimestamps();
     }
 
     public function authorizeRoles($roles)
--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -13,7 +13,7 @@ class RolesTableSeeder extends Seeder
     public function run()
     {
         $admin = new Role();
-        $admin->name        = 'Admin';
+        $admin->name        = 'admin';
         $admin->description = 'A admin user';
         $admin->save();
 
