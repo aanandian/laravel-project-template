f3a8b1edb1b8772426e56ee1f28a4f728b11a72a
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2018-11-01 07:47:56 +0700

    Add option lang

---
+++ resources/lang/id/options.php
@@ -0,0 +1,45 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Options Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for displaying options attribute
+    | labels that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    '_'         => 'Pilih',
+    '_all'      => 'Semua',
+
+    '_boolean'  => [
+        '0'         => 'Tidak',
+        '1'         => 'Ya',
+    ],
+
+    '_confirm'  => [
+        'no'        => 'No',
+        'yes'       => 'Yes',
+        'cancel'    => 'Batal',
+    ],
+
+    '_month'    => [
+        '01'    => 'Januari',
+        '02'    => 'Febuari',
+        '03'    => 'Maret',
+        '04'    => 'April',
+        '05'    => 'Mei',
+        '06'    => 'Juni',
+        '07'    => 'Juli',
+        '08'    => 'Agustus',
+        '09'    => 'September',
+        '10'    => 'Oktober',
+        '11'    => 'November',
+        '12'    => 'Desember',
+    ],
+
+];
