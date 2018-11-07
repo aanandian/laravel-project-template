904d841d8c72c9f07525eb5a0a94ead43f2256d1
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2018-11-08 04:36:57 +0700

    Remove unused line

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -135,6 +135,5 @@ class UserController extends Controller
             return redirect()->route('users.index')
                 ->with('success', __('messages.success-deleted'));
         }
-
     }
 }
