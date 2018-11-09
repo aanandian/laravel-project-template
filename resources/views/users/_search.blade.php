5f5cde080e975b2e2c114ffaacff9d27f41231e1
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2018-11-09 10:25:02 +0700

    Fix bug search form

--- resources/views/users/_search.blade.php
+++ resources/views/users/_search.blade.php
@@ -3,10 +3,10 @@
     <input id="_search" type="text" class="form-control form-control-sm" name="_search" value="{{ old('_search', $user->_search) }}" placeholder="@lang('models._search')">
 </div>
 
-<button type="submit" class="btn btn-secondary btn-sm ml-sm-2 mb-2">
+<button type="submit" class="btn btn-secondary btn-sm ml-2 mb-2">
     @lang('buttons.search')
 </button>
 
-<a class="btn btn-secondary btn-sm ml-sm-2 mb-2" href="{{ route(Route::currentRouteName()) }}">
+<a class="btn btn-secondary btn-sm ml-2 mb-2" href="{{ route(Route::currentRouteName()) }}">
     @lang('buttons.reset')
 </a>
