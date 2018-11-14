d7dbed72f3b5f9baa8d835db4bcacbd986770087
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2018-11-14 11:17:19 +0700

    Use role midlleware

---
+++ app/Http/Middleware/Role.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Role
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string  $accept
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $accepts)
+    {
+        $request->user()->authorizeRoles($accepts);
+
+        return $next($request);
+    }
+}
