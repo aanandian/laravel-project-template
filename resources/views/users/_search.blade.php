4412600a141bfad09bca83802beb14983f119729
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2018-11-14 11:20:49 +0700

    Bestpractice search form

--- resources/views/users/_search.blade.php
+++ resources/views/users/_search.blade.php
@@ -1,12 +1,12 @@
-<div class="form-group mb-2">
+<div class="form-group mb-2 mr-2">
     <label for="_search" class="sr-only">@lang('models._search')</label>
     <input id="_search" type="text" class="form-control form-control-sm" name="_search" value="{{ old('_search', $user->_search) }}" placeholder="@lang('models._search')">
 </div>
 
-<button type="submit" class="btn btn-secondary btn-sm ml-2 mb-2">
+<button type="submit" class="btn btn-secondary btn-sm mr-2 mb-2">
     @lang('buttons.search')
 </button>
 
-<a class="btn btn-secondary btn-sm ml-2 mb-2" href="{{ route(Route::currentRouteName()) }}">
+<a class="btn btn-secondary btn-sm mb-2" href="{{ route(Route::currentRouteName()) }}">
     @lang('buttons.reset')
 </a>
